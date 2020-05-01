a90357eb60f86565135fe831d20028c7ad1ef18a
karthickjagan90 <webdeveloper25m@gmail.com>
2020-05-01 12:22:56 +0530

    sample ui

---
+++ Mycomponent.js
@@ -0,0 +1,30 @@
+// CheckboxWithLabel.js
+
+import React from 'react';
+
+export default class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+   
+
+    // bind manually because React class components don't auto-bind
+ 
+  }
+
+  
+
+  render() {
+    return (
+      <a href="/about">About ℹ️</a>
+    
+    );
+  }
+}
+
+/*
+
+<form>
+       <label >Sign In
+            <input placeholder="Username">welcome</input>
+      </label>
+      </form>*/
