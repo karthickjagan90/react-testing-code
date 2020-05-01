a1a10b0f046dbf94a31a3866cdeb7be6a440bfff
karthickjagan90 <webdeveloper25m@gmail.com>
2020-05-01 12:24:22 +0530

    ui code

---
+++ Page.js
@@ -0,0 +1,24 @@
+// CheckboxWithLabel.js
+
+import React from 'react';
+
+export default class Page extends React.Component {
+  constructor(props) {
+    super(props);
+   
+
+    // bind manually because React class components don't auto-bind
+ 
+  }
+
+  
+
+  render() {
+    return (
+        <div>
+        <h1>Hello, world!</h1>
+        <h2>welcome</h2>
+        </div>
+    );
+  }
+}
