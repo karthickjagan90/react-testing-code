8f084d0e61f9708af58d4ee0a6ad94173fd651f3
karthickjagan90 <webdeveloper25m@gmail.com>
2020-05-01 12:22:35 +0530

    sample ui

---
+++ Greeting.js
@@ -0,0 +1,23 @@
+// CheckboxWithLabel.js
+
+import React from 'react';
+
+export default class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+   
+
+    // bind manually because React class components don't auto-bind
+ 
+  }
+
+  
+
+  render() {
+    return (
+        <div>
+        <h1>Hello, world!</h1>
+        </div>
+    );
+  }
+}
